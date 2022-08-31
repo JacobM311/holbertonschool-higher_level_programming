3bc2a51a30623430e1197174868a187055d98f93
JacobM311 <jacobmarshall9608@yahoo.com>
2022-08-31 10:44:23 -0700

    commit

--- 0x01-python-import_modules/0-add.py
+++ 0x01-python-import_modules/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 
-def pritn_add():
-    from add_0 import add
+from add_0 import add
+def print_add():
     a = 1
     b = 2
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
