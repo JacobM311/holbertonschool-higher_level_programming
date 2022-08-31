3c75b023c68571eff6814b31fac876a42b78cafa
JacobM311 <jacobmarshall9608@yahoo.com>
2022-08-31 12:27:06 -0700

    commit

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -2,13 +2,13 @@
 
 
 if __name__ == "__main__":
-from sys import argv
-l = len(argv)
-if l == 1:
-    print("{:d} argument:".format(l))
-elif l == 0:
-    print("0 arguments.")
-elif l > 1:
-    print("{:d} arguments:".format(l))
-for i, args in enumerate(argv):
-    print(f"{i}: {args}")
+    from sys import argv
+    l = len(argv)
+    if l == 1:
+        print("{:d} argument:".format(l))
+    elif l == 0:
+        print("0 arguments.")
+    elif l > 1:
+        print("{:d} arguments:".format(l))
+    for i, args in enumerate(argv):
+        print(f"{i}: {args}")
