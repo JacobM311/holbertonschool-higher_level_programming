367429908d3947f4973ba09d45a735dd07974ab8
JacobM311 <jacobmarshall9608@yahoo.com>
2022-09-12 21:48:04 -0700

    commit

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+    "hello"
+    def first_test(self):
+        v1 = [1, 2, 9, 76, 34]
+        self.assertTrue(max_integer(v1), 76)
