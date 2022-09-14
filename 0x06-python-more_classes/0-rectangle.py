7d61672e05fe9f4d9f1bc391221594c723939565
JacobM311 <jacobmarshall9608@yahoo.com>
2022-09-13 18:09:29 -0700

    commit

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -7,9 +7,9 @@ Defines a class Rectangle
 class Rectangle:
     """Representation of a rectangle"""
     def __init__(self, width=0, height=0):
-    """Initializes the rectangle"""
-    self.height = height
-    self.width = width
+        """Initializes the rectangle"""
+        self.height = height
+        self.width = width
 
     @property
     def width(self):
