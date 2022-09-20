13cde8c0e5a397a2ab544355424b83ce0e7c7265
JacobM311 <jacobmarshall9608@yahoo.com>
2022-09-19 19:44:41 -0700

    commit

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """
-Contains the "read_lines" function
+appends file and return characters
 """
 
 
 def read_lines(filename="", text=""):
     """reads file and appends"""
-    with open(filename, 'r', encoding='utf-8') as f:
+    with open(filename, mode="a", encoding='utf-8') as f:
         return(f.write(text))
