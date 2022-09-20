1bb73747fd5cb3198a0d6431db340ea772e854b7
JacobM311 <jacobmarshall9608@yahoo.com>
2022-09-20 10:56:20 -0700

    commit

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,14 +3,17 @@
 base class
 """
 
+class Base:
+    """
+    yessir
+    """
+    __nb_objects = 0
 
-__nb_objects = 0
+    def __init__(self, id=None):
+        "smelly"
 
-def __init__(self, id=None):
-    "smelly"
-
-    if id is None:
-        Base.__nb_objects += 1
-        self.id = Base.__nb_objects
-    else:
-        self.id = id
+        if id is None:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        else:
+            self.id = id
