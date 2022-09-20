0b32080fad3f95d0a86dca007615210e41563612
JacobM311 <jacobmarshall9608@yahoo.com>
2022-09-19 20:15:14 -0700

    commit

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 """
-returns the dictionary description with simple data structure (list, dictionary, string, integer and boolean) for JSON serialization of an object
+returns the dictionary description with
+simple data structure (list, dictionary,
+string, integer and boolean) for JSON serialization
+of an object
 """
 
 
 def class_to_json(obj):
-    """function that returns the dictionary description with simple data structure (list, dictionary, string, integer and boolean) for JSON serialization of an object"""
+    """function that returns the dictionary
+    description with simple data structure
+    (list, dictionary, string, integer and boolean)
+    for JSON serialization of an object"""
     return vars(obj)
