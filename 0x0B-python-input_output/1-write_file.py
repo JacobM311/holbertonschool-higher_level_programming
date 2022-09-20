39eb553ca961ab38202d7e58f24f721b78d01f7f
JacobM311 <jacobmarshall9608@yahoo.com>
2022-09-19 19:38:05 -0700

    commit

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -8,3 +8,6 @@ def write_file(filename="", text=""):
     """returns the number of lines of a text file"""
     with open(filename, 'r', encoding='utf8') as f:
         return(f.write(text))
+    i = 0
+        for i, line in enumerate(f):
+            print(line, end='')
