3d63f73b9394b1526ecb528595d8ec307cceb312
JacobM311 <jacobmarshall9608@yahoo.com>
2022-09-21 07:29:14 -0700

    commit

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,4 +4,15 @@ Rectangle that inherits from Base
 """
 
 
+from models.base import Base
 
+
+class Rectangle(Base):
+    """smelly"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initializes the rectangle"""
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
