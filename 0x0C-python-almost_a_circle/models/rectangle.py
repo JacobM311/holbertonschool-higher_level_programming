d9eeb7340773557594743658fbed8999648a1da8
JacobM311 <jacobmarshall9608@yahoo.com>
2022-09-22 13:25:04 -0700

    commit

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -107,5 +107,5 @@ class Rectangle(Base):
     def __str__(self):
         """string representation
         """
-        return "[Rectangle] ({:d}) {:d}/{:d} - {:d}/{:d}".format(self.id, self.__x, 
-                self.__y, self.__width, self.__height)
+        return "[Rectangle] ({:d}) {:d}/{:d} - {:d}/{:d}".format(self.id, self.__x,\
+-  self.__y, self.__width, self.__height)
