2379890cd422fb5504db03777e9eb31a72dd74f1
JacobM311 <jacobmarshall9608@yahoo.com>
2022-09-23 04:28:31 -0700

    commit

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module contains the "Square" class
+"""
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """stinky"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """initialize the square"""
+        super().__init__(size, size, x, y, id)
