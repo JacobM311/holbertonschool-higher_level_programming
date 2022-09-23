9a73976d8fb47221c233b6bc14e7fdd4b2e047a7
JacobM311 <jacobmarshall9608@yahoo.com>
2022-09-23 06:36:50 -0700

    commit

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -19,13 +19,15 @@ class Square(Rectangle):
         return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id, self.x,
                                                          self.y, self.width)
 
+    @property
+    def size(self):
+        """getter for size"""
+        return self.width
+
     @size.setter
     def size(self, value):
         """setter for size"""
         self.width = value
         self.height = value
 
-    @property
-    def size(self):
-        """getter for size"""
-        return self.width
+
