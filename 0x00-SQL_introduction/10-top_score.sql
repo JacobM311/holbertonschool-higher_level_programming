5157f8b344f30f13358ca94ad0ca8fde48236b0f
JacobM311 <jacobmarshall9608@yahoo.com>
2022-10-05 16:54:22 -0700

    lists all tables from second table

--- 0x00-SQL_introduction/10-top_score.sql
+++ 0x00-SQL_introduction/10-top_score.sql
@@ -1,5 +1,6 @@
--- lists all rows of the table first_table from the database hbtn_0c_0 in your MySQL server.
--- All fields should be printed
+-- lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
+-- Results should display both the score and the name (in this order)
+-- Records should be ordered by score (top first)
 -- The database name will be passed as an argument of the mysql command
 
-SELECT COUNT(*) FROM second_table;
+SELECT score, name FROM second_table ORDER BY score DESC;
