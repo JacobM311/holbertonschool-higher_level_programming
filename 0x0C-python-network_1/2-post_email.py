fe1a6dff34fb7ecb69e71d6f02595fb72d7ddcb4
JacobM311 <jacobmarshall9608@yahoo.com>
2022-11-01 19:29:44 -0700

    commit

---
+++ 0x0C-python-network_1/2-post_email.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+script sends a post request to given url and email & displays decoded
+body of the response
+"""
+from urllib.request import urlopen, Request
+from urllib.parse import urlencode
+from sys import argv
+
+
+if __name__ == '__main__':
+    givenurl = argv[1]
+    email = argv[2]
+    datadict = {'email': email}
+    encodeddata = urlencode(datadict)
+    utf8data = encodeddata.encode("utf-8")
+
+    with urlopen(givenurl, data=utf8data) as response:
+        body = response.read()
+        print(body.decode("utf-8"))
