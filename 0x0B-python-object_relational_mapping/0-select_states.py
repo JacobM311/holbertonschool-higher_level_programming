687b564a8a3f64a32fdde837f9ce51c514012d8c
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-02-27 16:32:09 -0600

    innit commit

--- 0x0B-python-object_relational_mapping/0-select_states.py
+++ 0x0B-python-object_relational_mapping/0-select_states.py
@@ -6,7 +6,7 @@ import sys
 
 def select_states():
     """ List all states from database hbtn_0e_0_usa """
-db = MySQLdb.connect(host='localhost', port=3306,
+    db = MySQLdb.connect(host='localhost', port=3306,
                          user=argv[1], passwd=argv[2], db=argv[3])
 
     cursor = db.cursor()
