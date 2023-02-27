0ea81f3f0d239f7dd06db1349b8af28b98f1137b
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-02-27 16:43:01 -0600

    innit commit

--- 0x0B-python-object_relational_mapping/0-select_states.py
+++ 0x0B-python-object_relational_mapping/0-select_states.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
-""" Script that list all states from the database hbtn_0e_0_usa"""
+"""This script grabs the username and password from user to print the table
+"""
 import MySQLdb
-from sys import argv
+import sys
 
-def select_states():
-    """ List all states from database hbtn_0e_0_usa """
-    db = MySQLdb.connect(host='localhost', port=3306,
-                         user=argv[1], passwd=argv[2], db=argv[3])
 
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    rows = cursor.fetchall()
-    for row in rows:
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
         print(row)
-    cursor.close()
-    db.close()
-
-if __name__=="__main__":
-    select_states()
+    cur.close()
+    conn.close()
