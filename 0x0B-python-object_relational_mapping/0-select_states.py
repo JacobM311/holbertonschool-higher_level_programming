a44328ced53d5461db79db3a3b704d8e18f8e172
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-02-27 16:31:08 -0600

    innit commit

--- 0x0B-python-object_relational_mapping/0-select_states.py
+++ 0x0B-python-object_relational_mapping/0-select_states.py
@@ -4,9 +4,9 @@
 import MySQLdb
 import sys
 
-
-if __name__ == "__main__":
-    db = MySQLdb.connect(host='localhost', port=3306,
+def select_states():
+    """ List all states from database hbtn_0e_0_usa """
+db = MySQLdb.connect(host='localhost', port=3306,
                          user=argv[1], passwd=argv[2], db=argv[3])
 
     cursor = db.cursor()
@@ -16,3 +16,6 @@ if __name__ == "__main__":
         print(row)
     cursor.close()
     db.close()
+
+if __name__=="__main__":
+    select_states()
