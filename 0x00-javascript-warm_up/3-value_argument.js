aeb39c4592ca4df67314c8448fb64ccb172449cb
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-04-08 08:32:27 -0500

    task 3

--- 0x00-javascript-warm_up/3-value_argument.js
+++ 0x00-javascript-warm_up/3-value_argument.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
-'prints the first argument passed to it'
+'prints the first argument passed to it';
 
 const firstArg = process.argv[2];
 
 if (firstArg !== undefined) {
-    console.log(firstArg)
-}
-
-else {
-    console.log("No argument")
+  console.log(firstArg);
+} else {
+  console.log('No argument');
 }
