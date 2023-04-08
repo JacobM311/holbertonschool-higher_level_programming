13906cb745a97e9e8ef3fbdda2a97af2c85bc59d
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-04-08 16:26:55 -0500

    task 6

--- 0x00-javascript-warm_up/6-multi_languages_loop.js
+++ 0x00-javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-'prints 3 lines by using an array of string and a loop'
+'prints 3 lines by using an array of string and a loop';
 
-let array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let i = 0; i < array.length; i++) {
-    console.log(array[i]);
+  console.log(array[i]);
 }
