e6f817d4359c069c51effce3c0ff8c3ab877ecff
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-04-08 08:05:02 -0500

    task 3

---
+++ 0x00-javascript-warm_up/2-arguments.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+'prints a message depending of the number of arguments passed';
+
+const process = require('process');
+
+const numArgs = process.argv.length - 2;
+
+if (numArgs === 0) {
+  console.log("No argument");
+} else if (numArgs === 1) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}
