64fcfeb5de981ed5c4dd28cad61c9d9ff58c06eb
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-04-09 13:19:20 -0500

    task 8 & 9

--- 0x00-javascript-warm_up/8-square.js
+++ 0x00-javascript-warm_up/8-square.js
@@ -6,7 +6,7 @@ const process = require('process');
 const input = process.argv[2];
 const size = parseInt(input, 10);
 
-if (isNaN(size) || size <= 0) {
+if (isNaN(size)) {
   console.log("Missing size");
 } else {
   for (let i = 0; i < size; i++) {
