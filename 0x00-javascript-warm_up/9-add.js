80120af9d5bd0bfdc89d8fa3fcf7100fa2bc8e65
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-04-09 13:22:48 -0500

    task 8 & 9

--- 0x00-javascript-warm_up/9-add.js
+++ 0x00-javascript-warm_up/9-add.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-'prints the addition of 2 integers'
-
+'prints the addition of 2 integers';
 
 const process = require('process');
 
@@ -8,7 +7,7 @@ if (process.argv.length >= 4) {
   const a = parseInt(process.argv[2], 10);
   const b = parseInt(process.argv[3], 10);
 
-  function add(a, b) {
+  function add (a, b) {
     return a + b;
   }
 
