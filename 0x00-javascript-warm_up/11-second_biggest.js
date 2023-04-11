3778017812ad0c500e0421758c3b7eb039e878bc
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-04-11 08:54:31 -0500

    task 11

---
+++ 0x00-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+'returns second largest of an array'
+
+const process = require('process');
+
+function secondLargest(numbers) {
+  let largest = Number.MIN_SAFE_INTEGER;
+  let secondLargest = Number.MIN_SAFE_INTEGER;
+
+  for (const number of numbers) {
+    if (number > largest) {
+      secondLargest = largest;
+      largest = number;
+    } else if (number > secondLargest && number < largest) {
+      secondLargest = number;
+    }
+  }
+
+  return secondLargest;
+}
+
+const args = process.argv.slice(2).map((arg) => parseInt(arg, 10));
+const result = args.length <= 1 ? 0 : secondLargest(args);
+
+console.log(result);
